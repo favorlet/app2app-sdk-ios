b175b372faafb89c9b77c8a051e3fe2dc58bd962
evahpirazzi <evahpirazzi@naver.com>
2022-12-05 21:39:47 +0900

    :chore 프로젝트 구조 변경 (Testing Framework NONE)

--- Example/Pods/Target Support Files/FavorletApp2App/FavorletApp2App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FavorletApp2App : NSObject
-@end
-@implementation PodsDummy_FavorletApp2App
-@end
--- Example/Pods/Target Support Files/Pods-FavorletApp2App_Tests/Pods-FavorletApp2App_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FavorletApp2App_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FavorletApp2App_Tests
-@end
